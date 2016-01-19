0fbc51392e987c6587908122e1c28fa24ed35540
Brandon Mrsly <bmrsny@gmail.com>
2016-01-18 22:20:25 -0700

    more comments clean up

--- app/components/trash-can/component.js
+++ app/components/trash-can/component.js
@@ -8,12 +8,5 @@ export default Ember.Component.extend({
   drop: function(e){
     var id = e.dataTransfer.getData('text/data');
     this.sendAction('removeFile', id);
-    // var record = this.get('file').findBy('id');
-    // record.set("inFolder", false).save().then(() => {
-      // console.log('save successful');
-      // this.refresh();
-    // }, function() {
-      // console.log('save failed')
-    // });
   }
 });
